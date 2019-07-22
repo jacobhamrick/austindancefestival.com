39a0d3951c5553c6358f53ecd3b23f3a6cccb82c
Jacob Hamrick <thejacobhamrick@gmail.com>
2019-07-22 11:01:07 -0400

    Start of major clean up and organization. HTML sections, CSS reorder, JS refactor.

--- js/adf.js
+++ js/adf.js
@@ -1,41 +1,41 @@
 $(document).ready(function() {
+
+  // expandable content icon switch
   $('.btn.btn-primary').click(function(){
     $(this).find('#classIcon').toggleClass('fa-plus fa-minus')
   });
 
-  //mobile to desktop navigation switch function
-  var menuSwitch = function() {
-    var windowWidth = $(window).width();
-    console.log(windowWidth);
-    var windowHeight = $(window).height();
-    //14px discrepancy, 829px - 14px = 815px (media query from CSS - 14px)
+  // mobile to desktop navigation switch function
+  const menuSwitch = function() {
+    let windowWidth = $(window).width();
+    let windowHeight = $(window).height();
+    // 14px discrepancy, 829px - 14px = 815px (media query from CSS - 14px)
     if (windowWidth < 815 || windowHeight < 450) {
-      $(".main-nav").addClass("smallScreen");
-      $(".main-nav").css("display", "none");
-      //resets light back to black on window resize
+      $("nav").addClass("smallScreen");
+      $("nav").css("display", "none");
+      // resets light back to black on window resize
       $("#menuIcon").removeClass('menuLight');
-      $(".scrollLink").click(function() {
-        $(".smallScreen").hide();
-      });
     } else {
-      $(".main-nav").removeClass("smallScreen");
-      //keeps scrollLinks from disappearing on window resize
-      $(".main-nav").css("display", "block");
+      $("nav").removeClass("smallScreen");
+      // keeps scrollLinks from disappearing on window resize
+      $("nav").css("display", "block");
     };
   }
-  //check window size and rerun function
+
+  // check window size and rerun function
   $(window).resize(function(){
     menuSwitch();
   });
-  //fire when ready
+
+  // fire when ready
   menuSwitch();
-  //mobile menu activiation
-  $("#menu").click(function(){
-    $(".main-nav").toggle();
+  // mobile menu activiation
+  $("#menuButton").click(function(){
+    $("nav").toggle();
     $("#menuIcon").toggleClass('menuLight');
   });
-  //swiper init
-  var mySwiper = new Swiper ('.swiper-container', {
+  // swiper init
+  const mySwiper = new Swiper ('.swiper-container', {
     direction: 'horizontal',
     loop: true,
     pagination: '.swiper-pagination',
@@ -43,43 +43,45 @@ $(document).ready(function() {
     prevButton: '.swiper-button-prev'
   });
 
-  //animated scroll link
-
-    $('.scrollLink').click(function(evn) {
-      evn.preventDefault();
-      const mobileOffset = 2;
-      const largeOffset = -24;
-      if ($(window).width() <= 829) {
+  // animated scroll link
+  $('.scrollLink').click(function(evn) {
+    evn.preventDefault();
+    const mobileOffset = 2;
+    const largeOffset = -24;
+    if ($(window).width() <= 829) {
+      $('html, body').animate({
+          scrollTop: $( $(this).attr('href') ).offset().top + mobileOffset
+      }, 600); // 2 = top border of section body
+      $("#menuIcon").toggleClass('menuLight'); //turns mobile menu light off
+      $("nav.smallScreen").css("display", "none");
+      return false;
+    } else {
         $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top + mobileOffset
-          }, 600); // 2 = top border of section body
-          $("#menuIcon").toggleClass('menuLight'); //turns mobile menu light off
+            scrollTop: $( $(this).attr('href') ).offset().top + largeOffset
+        }, 300, 'linear'); // 24 = height of newsFlash
         return false;
-      } else {
-          $('html, body').animate({
-              scrollTop: $( $(this).attr('href') ).offset().top + largeOffset
-            }, 300, 'linear'); // 24 = height of newsFlash
-            return false;
-          }
-        });
+    }
 
-  var navChildren = $(".scrollLink");
-      var aArray = [];
-      for (var i=0; i < navChildren.length; i++) {
-          var aChild = navChildren[i];
-          var ahref = $(aChild).attr('href');
-          aArray.push(ahref);
+  });
+
+  const navChildren = $(".scrollLink");
+  const aArray = [];
+
+  for (let i=0; i < navChildren.length; i++) {
+    let aChild = navChildren[i];
+    let ahref = $(aChild).attr('href');
+    aArray.push(ahref);
   }
 
   $(window).scroll(function(){
-    var windowPos = $(window).scrollTop() + (100);
-    var windowHeight = $(window).height();
-    var docHeight = $(document).height();
-
-    for (var i=0; i < aArray.length; i++) {
-        var theID = aArray[i];
-        var divPos = $(theID).offset().top;
-        var divHeight = $(theID).height();
+    let windowPos = $(window).scrollTop() + (100);
+    let windowHeight = $(window).height();
+    let docHeight = $(document).height();
+    console.log(`windowPos: ${windowPos}, windowHeight: ${windowHeight}, docHeight: ${docHeight}`)
+    for (let i=0; i < aArray.length; i++) {
+        const theID = aArray[i];
+        let divPos = $(theID).offset().top;
+        let divHeight = $(theID).height();
         //change active class for scrollLinks when scrolling thru sections
         if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
             $("a[href='" + theID + "']").addClass("active");
